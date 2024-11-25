3389aec71b04657bdd1af5f2b354cc249ee01e40
zhangjihang <zhangjihang@bytedance.com>
2024-11-25 19:37:10 +0800

    add mlu backend & add a config option(enable_vendor_compiler) to control

--- xpu_graph/backends/__init__.py
+++ xpu_graph/backends/__init__.py
@@ -4,12 +4,19 @@ from xpu_graph.utils import logger
 from typing import Callable
 
 def make_graph(gm: torch.fx.GraphModule, fake_inputs: list, target: Target) -> Callable:
+
     if target == Target.ascend:
         from .ascend import npu_compile
         logger.info("npu_compile start...")
         npu_compiled = npu_compile(gm, fake_inputs)
         logger.info("npu_compile complete")
         return npu_compiled
+    elif target == Target.mlu:
+        from .mlu import mlu_compile
+        logger.info("mlu_compile start...")
+        mlu_compiled = mlu_compile(gm, fake_inputs)
+        logger.info("mlu_compile complete")
+        return mlu_compiled
 
     return gm
 
