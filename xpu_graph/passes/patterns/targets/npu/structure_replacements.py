08eb6834d1730f4f1173d36cced6784c002d8bd7
mingfa-w <wangmingfa@bytedance.com>
2025-03-06 07:48:14 +0000

    [Ascend] fix: cast weight&bias if they does not match input in LayerNormModule

--- xpu_graph/passes/patterns/targets/npu/structure_replacements.py
+++ xpu_graph/passes/patterns/targets/npu/structure_replacements.py
@@ -4,6 +4,12 @@ import torch_npu
 
 class LayerNormModule(torch.nn.Module):
     def forward(self, input, weight, bias, epsilon):
+        if weight is not None:
+            if (input.dtype != weight.dtype):
+                weight = weight.to(input.dtype)
+        if bias is not None:
+            if (input.dtype != bias.dtype):
+                bias = bias.to(input.dtype)
         return torch.nn.functional.layer_norm(
             input, input.shape[-1:], weight, bias, epsilon
         )
