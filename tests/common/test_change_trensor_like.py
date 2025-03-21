dbfda5df0d8e8878f5dcac2306d36242bf0e3c1a
yfchen-byted <chenyifan.42@bytedance.com>
2025-03-22 01:55:36 +0800

    fix(SymInt): construct test case with symint shapes

--- tests/common/test_change_trensor_like.py
+++ tests/common/test_change_trensor_like.py
@@ -16,8 +16,24 @@ def fn2(a):
     output = torch.ones_like(a)
     return output
 
+
+def inner_fn(zeros, ones):
+        zeros = torch.zeros_like(zeros)
+        ones = torch.ones_like(ones)
+        output = torch.concat([zeros, ones], dim=0)
+        return output
+    
+def fn3(a):
+    b = torch.sum(a, dim=1)
+    outputs = []
+    for i in range(10):
+        output = inner_fn(a[b>=i], a[b<i])
+        outputs.append(output)
+    output = torch.stack(outputs, dim=0)
+    return output
+
 def tensorlike_test(xpu_graph, func):
-    compiled = torch.compile(func, backend=xpu_graph, dynamic=False)
+    compiled = torch.compile(func, backend=xpu_graph, dynamic=None)
     a = torch.randn(128, 64)
     res = func(a)
     res1 = compiled(a)
@@ -35,12 +51,15 @@ class TestTensorLike:
         [
             fn0,
             fn1,
+            fn2,
+            fn3,
         ],
     )
     def test_tensorlike_patterns(self, caplog, pattern_func):
         with need_xpu_graph_logs(), skip_xpu_graph_cache(self.xpu_graph):
             tensorlike_test(self.xpu_graph, pattern_func)
-        assert "Pattern.ChangeTensorLike changed graph" in caplog.text
+        if pattern_func not in [fn3]:
+            assert "Pattern.ChangeTensorLike changed graph" in caplog.text 
 
 
 if __name__ == "__main__":
@@ -48,3 +67,5 @@ if __name__ == "__main__":
     xpu_graph = xpu_graph.compiler.XpuGraph(config)
     tensorlike_test(xpu_graph, fn0)
     tensorlike_test(xpu_graph, fn1)
+    tensorlike_test(xpu_graph, fn2)
+    tensorlike_test(xpu_graph, fn3)
