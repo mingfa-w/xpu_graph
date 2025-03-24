2be53d79e17b44ce1eac18eaec4103d0fe19decb
yfchen-byted <chenyifan.42@bytedance.com>
2025-03-24 18:07:59 +0800

    feat(Logger): add finegrained logger (#103)

--- xpu_graph/utils.py
+++ xpu_graph/utils.py
@@ -2,24 +2,58 @@ import logging
 import sys
 import time
 import functools
+import os
 
-logger = logging.getLogger("XPU_GRAPH")
+
+class _LoggerWrapper:
+    def __init__(self, logger):
+        self._logger = logger
+
+    def __getattr__(self, name):
+        return getattr(self._logger, name)
+
+
+logger = _LoggerWrapper(logging.getLogger("xpu_graph"))
 
 
 def setup_logger(loglevel):
-    # Skip if handlers already exist
-    if logger.handlers:
-        return
+    if not logger.handlers:
+        # Skip if handlers already exist
+        fmt = logging.Formatter(
+            fmt="%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d [XPU_GRAPH][%(levelname)s]: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler = logging.StreamHandler(stream=sys.stdout)
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+        logger.propagate = False
 
-    fmt = logging.Formatter(
-        fmt="%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d [XPU_GRAPH][%(levelname)s]: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setFormatter(fmt)
-    logger.addHandler(handler)
     logger.setLevel(loglevel)
-    logger.propagate = False
+
+
+_debug_entries = [
+    "xpu_graph." + name for name in os.getenv("XPUGRAPH_LOGS", "").split(",")
+]
+
+
+class local_logger:
+    def __init__(self, name, level=None):
+        self.name = name
+        self.logger = logger.getChild(self.name)
+
+        if self.logger.name in _debug_entries:
+            level = logging.DEBUG
+        self.level = level or logger.level
+
+    def __enter__(self):
+        self.orig_logger = logger._logger
+        self.logger.setLevel(self.level)
+        logger._logger = self.logger
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        logger._logger = self.orig_logger
+        self.logger = None
+        self.orig_logger = None
 
 
 def xpu_timer(func):
