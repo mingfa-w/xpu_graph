e34b5ced53b1138fcaaf9c3c01f83085a87cd65b
jyjyjyjyjyjyjyj <a17621680413@163.com>
2025-03-26 20:30:40 +0800

    feat(mlu layernorm): erase cast before layernorm and after layernorm     *add pregrad pattern     *move pattern from common to mlu     *in qianchuan training, speedup 284->280     *add testcase

---
+++ xpu_graph/passes/patterns/targets/mlu/remove_layernorm_cast_pregrad.py
@@ -0,0 +1,35 @@
+import torch
+import torch.fx as fx
+from xpu_graph.fx_utils import FxStage
+from xpu_graph.config import OptLevel
+from xpu_graph.passes.patterns.pattern import AutoMatchPattern
+
+class RemoveLayerNormCastPregrad(AutoMatchPattern):
+    _opt_level = OptLevel.level2
+    _stages = [FxStage.pregrad]
+
+    def rewriter(self, gm: fx.GraphModule, rule_name: str, node_map: dict) -> bool:
+        assert (len(node_map) == 3)
+        pre_cast = node_map['pre_cast']
+        layernorm = node_map['layernorm']
+        post_cast = node_map['post_cast']
+
+        def _can_remove(pre_cast: fx.Node, post_cast: fx.Node) -> bool:
+            inp = pre_cast.args[0]
+            if inp.meta['tensor_meta'].dtype not in (torch.bfloat16, torch.float16,):
+                return False
+            if pre_cast.args[1] != torch.float32:
+                return False
+            if post_cast.args[1] != inp.meta['tensor_meta'].dtype:
+                return False
+            return True
+
+        if not _can_remove(pre_cast, post_cast):
+            return False
+
+        layernorm.replace_input_with(layernorm.args[0], pre_cast.args[0])
+        post_cast.replace_all_uses_with(layernorm)
+
+        gm.graph.lint()
+        gm.recompile()
+        return True
