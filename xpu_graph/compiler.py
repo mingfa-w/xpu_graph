a2e43db78bda476514a0d2daccd6bcf5d9cd91c7
mingfa-w <wangmingfa@bytedance.com>
2025-03-31 08:11:09 +0000

    fixed compile bug on ascend npu

--- xpu_graph/compiler.py
+++ xpu_graph/compiler.py
@@ -101,7 +101,7 @@ class XpuGraph:
                     if xpu_compiled is None:
                         xpu_compiled = self._pass_manager(gm, fake_inputs)
                         # xpu_compiled = self._cache.save_gm(hashkey, xpu_compiled)
-                        if self._config.target != Target("ascend"):
+                        if self._config.target != Target("npu"):
                             xpu_compiled = self._cache.save_gm(hashkey, xpu_compiled)
                 else:
                     xpu_compiled = self._pass_manager(gm, fake_inputs)
