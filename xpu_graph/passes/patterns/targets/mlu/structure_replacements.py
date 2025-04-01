feat(structure): add split_fwd pattern.     *Fixed numerical accuracy issues in fused_slice when running with large batch sizes.     *On Model 1 inference: Reduced operator count by 38/Reduced kernel launches by 23/Achieved ~2%

xpu_graph/passes/patterns/targets/mlu/structure_replacements.py
@@ -10,9 +10,11 @@ from .triton_kernel.fused_slice_cat import (
     fused_slice_cat,
 )
 
+
 class RMSNormModule(torch.nn.Module):
     def forward(self, inputs, weights, epsilon):
         import torch_mlu_ops
+
         return torch_mlu_ops.fused_rms_norm(
             inputs, None, weights, None, None, epsilon, False
         )
@@ -33,10 +35,25 @@ class FuseSliceModule(torch.nn.Module):
             input_tensor,
             slices_index,
             slice_len,
-            input_tensor.shape[0],
-            input_tensor.stride(0),
         )
-        return output.view(len(slices_index), input_tensor.shape[0], slice_len)
+        return output
+
+
+class FuseSplitModule(torch.nn.Module):
+    def forward(self, x: torch.Tensor, split_size: int, dim: int):
+        if len(x.shape) != 2:
+            raise NotImplementedError("input must be 2d")
+        split_num = x.shape[1] // split_size
+        if split_num * split_size != x.shape[1]:
+            raise NotImplementedError("fused split don't support.")
+        slices_index = list(range(0, x.shape[1], split_size))
+        start_indices = torch.tensor(slices_index, device=x.device, dtype=torch.int32)
+        output = fused_slice_low(
+            x,
+            start_indices,
+            split_size,
+        )
+        return output.unbind(0)
 
 
 class FuseSliceCatSameInputModule(torch.nn.Module):
@@ -62,6 +79,7 @@ def get_structure_replacements():
     return {
         "FusedRMSNorm": RMSNormModule,
         "FusedSlice": FuseSliceModule,
+        "FusedSplit": FuseSplitModule,
         "FusedCatSlice": FuseSliceCatSameInputModule,
         "FusedMultipleSliceCat": FuseSliceCatSameInputModule,
     }

xpu_graph/passes/patterns/targets/mlu/triton_kernel/fused_slice.py
@@ -22,15 +22,20 @@ def mlu_triton_slice_low_kernel(
     program_dim = tl.num_programs(axis=0)
     program_id = tl.program_id(0)
     block_jobs = total_jobs // program_dim
-    block_jobs_r = block_jobs
-    if program_id == program_dim - 1:
-        block_jobs_r = total_jobs - block_jobs * (program_dim - 1)
+    remainder = total_jobs % program_dim
+    if program_id < remainder:
+        block_jobs_r = block_jobs + 1
+        offset = program_id * (block_jobs + 1)
+    else:
+        block_jobs_r = block_jobs
+        offset = remainder * (block_jobs + 1) + (program_id - remainder) * block_jobs
 
     loop = (input_row + BLOCK_SIZE_R - 1) // BLOCK_SIZE_R
     for l in range(loop):
         for block_idx in range(block_jobs_r):
-            slice_idx = block_idx + block_jobs * program_id
+            slice_idx = block_idx + offset
             start_index = tl.load(start_indices_ptr + slice_idx)
+            output_ptr_offset = output_ptr + (slice_idx * input_row) * slice_len
 
             input_block_ptr = tl.make_block_ptr(
                 base=input_ptr,
@@ -42,10 +47,10 @@ def mlu_triton_slice_low_kernel(
             )
             value = tl.load(input_block_ptr, boundary_check=(0,), padding_option=0)
             output_block_ptr = tl.make_block_ptr(
-                base=output_ptr,
-                shape=(input_row * total_jobs, slice_len),
+                base=output_ptr_offset,
+                shape=(input_row, slice_len),
                 strides=(slice_len, 1),
-                offsets=(slice_idx * input_row + l * BLOCK_SIZE_R, 0),
+                offsets=(l * BLOCK_SIZE_R, 0),
                 block_shape=(BLOCK_SIZE_R, BLOCK_SIZE_C),
                 order=(1, 0),
             )
@@ -57,9 +62,9 @@ def fused_slice_low(
     src_tensor: torch.Tensor,
     start_indices: torch.Tensor,
     slice_len: int,
-    n_rows: int,
-    input_stride: int,
 ) -> torch.Tensor:
+    n_rows = src_tensor.shape[0]
+    input_stride = src_tensor.stride(0)
     block_size_r = n_rows
     block_size_c = slice_len
     size_of_dtype = 2
@@ -91,13 +96,13 @@ def fused_slice_low(
         block_size_c,
     )
 
-    return output_tensors
+    return output_tensors.view(num_slices, src_tensor.shape[0], slice_len)
 
 
 @fused_slice_low.register_fake
-def fused_slice_low_fake(src_tensor, start_indices, slice_len, n_rows, input_stride):
+def fused_slice_low_fake(src_tensor, start_indices, slice_len):
     output_tensors = torch.empty(
-        (len(start_indices) * src_tensor.shape[0], slice_len),
+        (len(start_indices), src_tensor.shape[0], slice_len),
         device=src_tensor.device,
         dtype=src_tensor.dtype,
     )