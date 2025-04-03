672d9e8d40c4c460a86c42c81fe81f35be023d15
mingfa-w <wangmingfa@bytedance.com>
2025-04-03 18:39:08 +0800

    Revert "feat(ascend compiler): Enable xpu_graph + ge compiler for npu-910b"
    This reverts commit bb1a6de9aac01f3f8a73001ccfeed46ef5647d98.

--- xpu_graph/backends/npu.py
+++ xpu_graph/backends/npu.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from typing import Dict
 import torch
 
@@ -18,26 +17,3 @@ def npu_compile(
     )
 
     return compiled_module
-=======
-import torch
-import torch.fx as fx
-
-def npu_compile(module: torch.nn.Module, example_inputs) -> torch.nn.Module:
-    torch.npu.set_compile_mode(jit_compile=False)
-
-    import torchair as tng
-    import torchair.ge_concrete_graph.ge_converter.experimental.patch_for_hcom_allreduce
-    from torchair.configs.compiler_config import CompilerConfig
-
-    """
-    TODO(zhangjihang): We have to use this, cause some case we have to use GE
-    """
-    config = CompilerConfig()
-    config.experimental_config.keep_inference_input_mutations = True
-    config.experimental_config.frozen_parameter = True
-
-    npu_backend = tng.get_npu_backend(compiler_config=config)
-    compiled_module = npu_backend(module, example_inputs)
-
-    return compiled_module
->>>>>>> c6888e1 (feat(ascend compiler): Enable xpu_graph + ge compiler for npu-910b)
