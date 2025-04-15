9ad96978a9fc04651458c048d2872d5a5e18ab2e
wxue <wangxue125@huawei.com>
2025-04-15 10:25:37 +0800

    feat(npu_compiler): add npu_compiler for deberta
      * Support npu_compiler for deberta model
      * Add two patterns

--- xpu_graph/__init__.py
+++ xpu_graph/__init__.py
@@ -35,6 +35,25 @@ def ascend_compiler(
     )
     return XpuGraph(config, cache)
 
+def npu_compiler(
+    freeze: bool = False,
+    opt_level: OptLevel = OptLevel.level1,
+    constant_folding: bool = True,
+    cache: XpuGraphCache = default_cache(),
+    debug: bool = False,
+    vendor_compiler_config: Dict[str, Any] = {"mode": "default"},
+):
+    config = XpuGraphConfig(
+        is_training=False,
+        target=Target.npu,
+        freeze=freeze,
+        opt_level=opt_level,
+        constant_folding=constant_folding,
+        debug=debug,
+        vendor_compiler_config=vendor_compiler_config,
+    )
+    return XpuGraph(config, cache)
+
 def mlu_compiler(
     is_training: bool,
     **patch_configs,
