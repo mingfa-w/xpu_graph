9698da97ba5a9048a28673c4d7ae3685a31cf991
saruagithub <13452916191@163.com>
2025-04-18 15:11:33 +0800

    Fix(npu compiler): Fix pattern registration
      * npu pattern register

---
+++ xpu_graph/passes/patterns/targets/npu/__init__.py
@@ -0,0 +1,33 @@
+import pkgutil
+import importlib
+
+from xpu_graph.passes.patterns.pattern import Pattern, AutoMatchPattern, PatternGroup
+from xpu_graph.config import XpuGraphConfig
+from xpu_graph.utils import logger
+
+
+def get_all_patterns(config: XpuGraphConfig):
+    patterns = {
+        PatternGroup.GROUP0: [],
+        PatternGroup.GROUP1: [],
+        PatternGroup.GROUP2: [],
+    }
+
+    for _, module_name, _ in pkgutil.iter_modules(__path__):
+        module = importlib.import_module(f"{__name__}.{module_name}")
+
+        for name in dir(module):
+            pat = getattr(module, name)
+            if (
+                isinstance(pat, type)
+                and issubclass(pat, Pattern)
+                and pat not in (Pattern, AutoMatchPattern)
+                and pat._opt_level <= config.opt_level
+            ):
+                for stage in pat._stages:
+                    patterns[pat._pattern_group].append(pat(stage))
+
+    for group, group_patterns in patterns.items():
+        logger.debug(f"xpu_graph enable builtin npu {group} patterns: {[pat.__class__.__name__ for pat in group_patterns]}")
+
+    return patterns
