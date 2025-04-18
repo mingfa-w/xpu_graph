Fix(FusedBrcPermuteSum fail): fail pattern match due to softmax (#173)
* fix _softmax op match check fail

tests/npu/test_fuse_permute_sum.py
@@ -1,11 +1,8 @@
 import torch
-import torch_npu
-import inductor_npu
 import xpu_graph
 from xpu_graph.config import OptLevel
 from xpu_graph.test_utils import is_similar
-
-
+import torch_npu
 
 def fn0(view_7, gather, gather_1, arg107_1, full_default):
     slice_3: "i64[11, 256]" = torch.ops.aten.slice.Tensor(arg107_1, 1, 0, 512)

xpu_graph/cache.py
@@ -8,7 +8,13 @@ from .fx_utils import FxStage
 import torch
 from torch._dynamo.convert_frame import compile_lock
 from torch.utils._python_dispatch import _disable_current_modes
-from torch._inductor.codecache import CompiledFxGraph, PyCodeCache, get_path
+
+torch_version = torch.__version__
+if torch_version.startswith("2.6"):
+    from torch._inductor.compile_fx import CompiledFxGraph
+    from torch._inductor.codecache import PyCodeCache, get_path
+else:
+    from torch._inductor.codecache import CompiledFxGraph, PyCodeCache, get_path
 from torch.fx import GraphModule, Graph, Node
 from torch.fx.node import map_aggregate
 import copy