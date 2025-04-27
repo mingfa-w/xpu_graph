5099821b36b7800a465aee0d1d4aa5cdd807c0c5
yeyeye333 <yhjyyy2024@163.com>
2025-04-27 19:32:48 +0800

    feat(repeat2expand): change repeat to expand     * Add: patterns/targets/mlu/repeat_to_expand.py

--- tests/mlu/test_repeat_to_expand.py
+++ tests/mlu/test_repeat_to_expand.py
@@ -1,5 +1,6 @@
 import math
 import pytest
+import random
 
 import torch
 import xpu_graph
@@ -17,16 +18,27 @@ def fn0(input, input_num, bs, in_dim):
     sqa = sqa.unsqueeze(-1).repeat(1, 1, in_dim)
     return input.gather(index=sqa, dim=1)
 
+def fn1(input, a, b):
+    output = input.repeat(1, 32)
+    output = torch.where(output, a, b)
+    return output
 
 def gather_test(xpu_graph_backend, func):
-    batch = 86
-    in_dim = 256
-    dtype = torch.half
-    input = torch.randn(batch, 496, in_dim, dtype=dtype, device="mlu")
-    res1 = func(input, 46, batch, in_dim)
     compiled = torch.compile(func, backend=xpu_graph_backend, dynamic=False)
-    res = compiled(input, 46, batch, in_dim)
-
+    dtype = torch.half
+    if func == fn0:
+        batch = 86
+        in_dim = 256
+        input = torch.randn(batch, 496, in_dim, dtype=dtype, device="mlu")
+        res1 = func(input, 46, batch, in_dim)
+        res = compiled(input, 46, batch, in_dim)
+    if func == fn1:
+        random_list = random.choices([0, 1], k=86)
+        input = torch.tensor(random_list, device="mlu").unsqueeze(-1).bool()
+        a = torch.randn(86, 32, dtype=dtype, device="mlu")
+        b = torch.randn(86, 32, dtype=dtype, device="mlu")
+        res1 = func(input, a, b)
+        res = compiled(input, a, b)
     assert torch.equal(res.cpu().float(), res1.cpu().float())
 
 
@@ -38,12 +50,18 @@ class TestGather:
 
     @pytest.mark.parametrize(
         "pattern_func",
-        [fn0],
+        [
+            fn0,
+            fn1
+        ],
     )
     def test_gather_patterns(self, caplog, pattern_func):
         with need_xpu_graph_logs(), skip_xpu_graph_cache(self.xpu_graph_backend):
             gather_test(self.xpu_graph_backend, pattern_func)
-        assert "Pattern.FusedGatherToCopy changed graph" in caplog.text
+        if pattern_func == fn0:
+            assert "Pattern.FusedGatherToCopy changed graph" in caplog.text
+        if pattern_func == fn1:
+            assert "Pattern.Repeat2Expand changed graph" in caplog.text
 
 
 if __name__ == "__main__":
@@ -51,3 +69,4 @@ if __name__ == "__main__":
         is_training=False, debug=True, opt_level=OptLevel.level1
     )
     gather_test(xpu_graph_backend, fn0)
+    gather_test(xpu_graph_backend, fn1)
--- xpu_graph/passes/patterns/targets/mlu/repeat_to_expand.py
+++ xpu_graph/passes/patterns/targets/mlu/repeat_to_expand.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, List
 
 import torch
 from torch import nn, fx
@@ -9,6 +9,8 @@ from xpu_graph.passes.patterns.pattern import Pattern
 from xpu_graph.fx_utils import FxStage
 from ...utils.check_ops import (
     get_shape,
+    check_where_op,
+    check_repeat_op,
 )
 
 TensorShape = Union[torch.Size, Tuple[int, ...]]
@@ -20,11 +22,9 @@ NodeType = fx.Node
     %repeat : [num_users=1] = call_function[target=torch.ops.aten.repeat.default](args = (%unsqueeze, [1, 1, 256]), kwargs = {})
     %gather : [num_users=1] = call_function[target=torch.ops.aten.gather.default](args = (%arg0_1, 1, %repeat), kwargs = {})
 """
-
-
 class FusedGatherToCopy(Pattern):
     _opt_level = OptLevel.level1
-    _support_stages = [FxStage.inference, FxStage.pregrad]
+    _stages = [FxStage.inference, FxStage.pregrad]
 
     def process(self, graph_module: fx.GraphModule) -> bool:
         is_modified = False
@@ -57,4 +57,67 @@ class FusedGatherToCopy(Pattern):
             is_modified = True
             graph_module.graph.lint()
             graph_module.recompile()
+        return is_modified
+
+
+"""
+    sample_code:
+    %repeat : [num_users=7] = call_function[target=torch.ops.aten.repeat.default](args = (%logical_or_13, [1, 32]), kwargs = {})
+    %where_61 : [num_users=1] = call_function[target=torch.ops.aten.where.self](args = (%repeat, %add_40_replacement, %add_38_replacement), kwargs = {})
+"""
+
+def _is_repeat2expand(
+    node: fx.Node,
+) -> tuple[bool, Optional[fx.Node], Optional[List]]:
+    if not check_where_op(node):
+        return False, None, []
+
+    repeat_node = node.args[0]
+    if not check_repeat_op(repeat_node):
+        return False, None, []
+
+    repeat_input = repeat_node.args[0]
+    repeat_param = repeat_node.args[1]
+    repeat_shape = repeat_input.meta["tensor_meta"].shape
+    expand_param = list(repeat_param)
+    if len(repeat_shape) == len(repeat_param):
+        for i in range(len(repeat_param)):
+            if repeat_param[i] != 1:
+                if repeat_shape[i] != 1:
+                    return False, None, []
+            elif repeat_param[i] == 1:
+                expand_param[i] = repeat_shape[i]
+    else:
+        for i in range(len(repeat_param)):
+            if repeat_param[i] == 1:
+                expand_param[i] = repeat_shape[i]
+            else:
+                return False, None, []
+
+    return True, repeat_node, expand_param
+
+
+class Repeat2Expand(Pattern):
+    _opt_level = OptLevel.level1
+    _stages = [FxStage.inference, FxStage.pregrad]
+
+    def process(self, gm: fx.GraphModule):
+        is_modified = False
+
+        for node in gm.graph.nodes:
+            is_match, repeat_node, expand_param = _is_repeat2expand(node)
+            if is_match:
+                with gm.graph.inserting_before(repeat_node):
+                    new_node = gm.graph.create_node(
+                        op="call_function",
+                        target=torch.ops.aten.expand.default,
+                        args=(repeat_node.args[0], expand_param),
+                        name=repeat_node.name + "_replacement",
+                    )
+                repeat_node.replace_all_uses_with(new_node)
+                gm.graph.erase_node(repeat_node)
+                is_modified = True
+
+        gm.graph.lint()
+        gm.recompile()
         return is_modified
