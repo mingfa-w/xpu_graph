fdc96e058a2d4bc5f9d4433750d2c8c837124e6e
煎茶捣药 <liuyuan.leo@bytedance.com>
2025-06-18 12:28:16 +0800

    chore(.github&lint_check.sh): update template files and refactor the use (#231)
    of pre-commit.

--- scripts/lint_check.sh
+++ scripts/lint_check.sh
@@ -9,6 +9,7 @@ cd $PROJ_DIR
 
 source ${PROJ_DIR}/scripts/install_githooks.sh
 
-pre-commit run --files $(git diff --diff-filter=ACMR --name-only ${TARGET_REF})
+# pre-commit run --files $(git diff --diff-filter=ACMR --name-only ${TARGET_REF})
+pre-commit run --from-ref ${TARGET_REF} --to-ref HEAD
 
 echo "Lint Check Succeed!"
