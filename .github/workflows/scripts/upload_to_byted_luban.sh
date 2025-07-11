5330d625e41109b8f8b64c13e9ed1ed396889704
mingfa-byted <wangmingfa@bytedance.com>
2025-07-11 18:56:08 +0800

    fix(ci): fix upload whl to luban token (#323)
    Co-authored-by: wangmingfa@bytedance.com <wangmingfa>

--- .github/workflows/scripts/upload_to_byted_luban.sh
+++ .github/workflows/scripts/upload_to_byted_luban.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
+set -e
 current_dir=`pwd`
 script_dir=$(cd $(dirname $0); pwd)
 BYTED_CLOUD_AICOMPILER_SECRET=$1
-
+OAuth2_Client_secret=$2
+PKG=byted-xpu-graph
 cd ${current_dir}/dist
 TAR_X86_64=`ls *.whl`
 WHL_X86_64=`ls *.tar*`
 echo release: $TAR_X86_64, $WHL_X86_64
 
-# 1. 获取 JWT_TOKEN
-JWT_TOKEN=`curl -s -I -X GET  https://cloud.bytedance.net/auth/api/v1/jwt -H "Authorization: Bearer ${BYTED_CLOUD_AICOMPILER_SECRET}" | awk -F': ' '/^x-jwt-token/ {print $2}'`
-
-if [ -n $JWT_TOKEN ]; then
-    echo JWT_TOKEN get success.
+# 1. 通过服务号获取个人 JWT_TOKEN
+# 1.1 获取平台 token
+TOKEN=$(curl -X GET 'https://cloud.bytedance.net/auth/api/v1/jwt' -H "Authorization: Bearer ${BYTED_CLOUD_AICOMPILER_SECRET}" -s -D - -o /dev/null | grep -i 'x-jwt-token' | awk '{print $2}' | tr -d '\r')
+if [ -n $TOKEN ]; then
+    echo get aicompiler token success.
 else
-    echo JWT_TOKEN get failed.
+    echo get aicompiler token failed.
+    exit 1
+fi
+
+# 1.2 用平台 token 换取个人 token
+JWT_TOKEN=$(curl -s --location --request POST 'https://cloud.bytedance.net/auth/api/v1/token' \
+--header "X-Jwt-Token: $TOKEN" \
+--header 'Content-Type: application/json' \
+        --data-raw "{
+         "client_id": "cli_XgwbKu66",
+         "client_secret": "$OAuth2_Client_secret",
+         "redirect_uri": "",
+         "grant_type": "authorization_code",
+         "auth_type": "custom",
+         "username": "wangmingfa",
+         "lark_auth_req": true
+ }" | jq -r '.access_token')
+if [ -n $JWT_TOKEN ]; then
+    echo get wangmingfa token success.
+else
+    echo get wangmingfa token failed.
     exit 1
 fi
 
 # 2. 上传版本到 byted pypi luban
-curl --location 'https://bytedpypi.byted.org/' \
+curl --http1.1 --location 'https://bytedpypi.byted.org/' \
 --header "X-Jwt-Token: ${JWT_TOKEN}" \
 --form "pypi.package=${PKG}" \
 --form "asset0.filename=${TAR_X86_64}" \
